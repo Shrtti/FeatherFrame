98bb6162124ce67d80c1579dab202a6d6dac0e9e
Sh Nate <snatekar03@gmail.com>
2025-06-15 20:11:21 +0530

    deploy vercel

--- next.config.js
+++ next.config.js
@@ -9,13 +9,20 @@ const nextConfig = {
         port: '5000',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.onrender.com',
+        pathname: '/**',
+      }
     ],
   },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+          : 'http://localhost:5000/api/:path*',
       },
     ]
   },
